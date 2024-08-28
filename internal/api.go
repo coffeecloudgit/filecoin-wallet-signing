9472f8ce4131210aafcc3043faf1f3488319b104
yangpro <y62069061@gmail.com>
2024-08-28 16:57:52 +0800

    update lotus api

--- internal/api.go
+++ internal/api.go
@@ -33,17 +33,24 @@ func ConnectLotus() error {
 	if lotusAPI == "" {
 		//lotusAPI = "https://api.node.glif.io/rpc/v1"
 		lotusAPI = "https://node.filutils.com/rpc/v1"
-		token = "I6505IXYTJWI9LG3EZXQCO7FBZ846QPWXQX"
+		token = "Bearer " + "I6505IXYTJWI9LG3EZXQCO7FBZ846QPWXQX"
 		//lotusAPI = "https://fil.coin.bit99.com:1443/rpc/v1"
 	}
 
 	fmt.Println("LOTUS_API : ", lotusAPI)
 	fmt.Println("LOTUS_API_TOKEN : ", token)
-
-	headers := http.Header{
-		//"Authorization": []string{token},
-		"content-type": []string{"application/json"},
+	var headers http.Header
+	if len(token) > 7 {
+		headers = http.Header{
+			"Authorization": []string{token},
+			"content-type":  []string{"application/json"},
+		}
+	} else {
+		headers = http.Header{
+			"content-type": []string{"application/json"},
+		}
 	}
+
 	closer, err := jsonrpc.NewMergeClient(context.Background(), lotusAPI, "Filecoin", []interface{}{&Lapi.Internal, &Lapi.CommonStruct.Internal}, headers)
 	if err != nil {
 		return fmt.Errorf("connecting with lotus failed: %s", err)
